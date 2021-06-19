1f46a6e21f22db7786c24fe3b833322d49555f2d
Antti Vuorenmaa <antti@vertics.co>
2021-06-19 17:44:34 +0300

    Exercises 3.19-3.21

--- index.js
+++ index.js
@@ -89,14 +89,6 @@ app.post('/api/persons', async (request, response, next) => {
         })
     }
 
-    const exists = (await Person.findOne({name: body.name})) !== null
-
-    if (exists) {
-        return response.status(409).json({
-            error: 'name must be unique'
-        })
-    }
-
     const person = new Person({
         name: body.name,
         number: body.number
@@ -118,7 +110,7 @@ app.put('/api/persons/:id', (request, response, next) => {
         number: body.number
     }
 
-    Person.findByIdAndUpdate(id, person, { new: true })
+    Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
         response.json(updatedPerson)
     })
@@ -132,10 +124,16 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+    console.error(error)
 
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        if (error.errors?.name?.kind === 'unique') {
+            console.log(error.errors)
+            return response.status(409).json({ error: `person with name ${error.errors?.name?.value} already exists` })
+        }
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
@@ -12,16 +13,28 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
 })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+    name: {
+        type: String,
+        minlength: 3,
+        required: true,
+        unique: true
+    },
+    number: {
+        type: String,
+        minlength: 8,
+        required: true,
+        unique: false
+    }
 })
 
+personSchema.plugin(uniqueValidator);
+
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 module.exports = mongoose.model('Person', personSchema)
