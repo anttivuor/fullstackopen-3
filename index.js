89963d17d9bca6ff5e552fab86aee0fa3b77e8b9
Antti Vuorenmaa <antti@vertics.co>
2021-06-19 15:00:00 +0300

    Heroku deployment

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
+app.use(express.static('build'))
 
 let phonebook = [
     {
@@ -93,7 +96,7 @@ app.post('/api/persons', (request, response) => {
     response.json(phonebook)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
